0f91b1525f7ef60325a8f79ee6f73aabf05c8702
remind1990 <tigerstylest00@gmail.com>
2024-09-01 13:40:16 -0230

    nav bar fix

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
@@ -10,6 +10,7 @@ import { useTranslations } from 'next-intl';
 type Props = {};
 
 const NavBar: React.FC<Props> = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
   const pathname = usePathname();
   const t = useTranslations('Navigation');
@@ -20,6 +21,10 @@ const NavBar: React.FC<Props> = () => {
     { name: t('portfolio'), path: '/portfolio' },
     { name: t('contacts'), path: '/contacts' },
   ];
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <header className="flex items-center justify-between p-2 main-bg text-stone-800">
@@ -32,26 +37,60 @@ const NavBar: React.FC<Props> = () => {
           className="mr-4 rounded-full cursor-pointer"
           onClick={() => router.push('/')}
         />
-        <nav>
-          <ul className="flex space-x-4">
-            {navItems.map((item) => (
-              <li key={item.path}>
-                <Link
-                  href={item.path}
-                  className={`${
-                    pathname === item.path ? 'text-stone-900' : 'text-stone-500'
-                  } hover:text-stone-700`}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="ml-auto">
-          <LocaleSwitcher />
+        <div className="flex items-center">
+          <button
+            className="ml-4 flex flex-col justify-between h-6 w-8 md:hidden focus:outline-none"
+            onClick={toggleMenu}
+          >
+            {/* Burger Menu Icon */}
+            <span className="block w-full h-[1px] bg-black"></span>
+            <span className="block w-full h-[1px] bg-black"></span>
+            <span className="block w-full h-[1px] bg-black"></span>
+          </button>
         </div>
       </div>
+
+      {/* Mobile Menu Dropdown */}
+      <nav
+        className={`${
+          isMenuOpen ? 'block' : 'hidden'
+        } md:hidden absolute top-12 left-0 w-full bg-white shadow-md z-10`}
+      >
+        <ul className="flex flex-col space-y-2 p-4">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <Link
+                href={item.path}
+                className={`${
+                  pathname === item.path ? 'text-stone-900' : 'text-stone-500'
+                } hover:text-stone-700`}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* Regular Desktop Menu */}
+      <nav className="hidden md:flex">
+        <ul className="flex space-x-4">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <Link
+                href={item.path}
+                className={`${
+                  pathname === item.path ? 'text-stone-900' : 'text-stone-500'
+                } hover:text-stone-700`}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <LocaleSwitcher />
     </header>
   );
 };
