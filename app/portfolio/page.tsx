78b6b1043460d10291f9d704a0e4905a7697547e
remind1990 <tigerstylest00@gmail.com>
2024-09-02 21:47:42 -0230

    new images

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Portfolio from '../pages/Portfolio/Portfolio';
+import { fetchPhotos } from '@/lib/fetchPhotos';
 
 type Props = {};
 
-const page = (props: Props) => {
-  return <Portfolio />;
+const page = async (props: Props) => {
+  const photos = await fetchPhotos();
+  return <Portfolio photos={photos} />;
 };
 
 export default page;
