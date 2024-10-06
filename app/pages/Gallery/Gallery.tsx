b11e63684903ff824d3746de69357899bd030704
remind1990 <tigerstylest00@gmail.com>
2024-10-06 20:36:28 -0230

    fix

--- app/pages/Gallery/Gallery.tsx
+++ app/pages/Gallery/Gallery.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ToolBar from './components/ToolBar';
 import Modal from '@/components/Modal';
 import Image from 'next/image';
@@ -8,53 +8,10 @@ import { usePhotoContext } from '@/app/context/PhotoContext';
 import { deletePhoto, PhotoData } from '@/lib/fetchPhotos';
 import Loader from '@/components/Loader';
 import { useTranslations } from 'next-intl';
-import { useDrag, useDrop, DndProvider } from 'react-dnd';
+import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { updatePhotoOrder } from '@/lib/updateMetaData';
-interface DragItem {
-  index: number;
-  type: string;
-}
-
-const ItemType = {
-  PHOTO: 'photo',
-};
-
-interface DraggablePhotoProps {
-  url: string;
-  index: number;
-  movePhoto: (fromIndex: number, toIndex: number) => void;
-}
-
-function DraggablePhoto({ url, index, movePhoto }: DraggablePhotoProps) {
-  const [, ref] = useDrag<DragItem>({
-    type: ItemType.PHOTO,
-    item: { index, type: ItemType.PHOTO },
-  });
-
-  const [, drop] = useDrop<DragItem>({
-    accept: ItemType.PHOTO,
-    hover: (draggedItem) => {
-      if (draggedItem.index !== index) {
-        movePhoto(draggedItem.index, index);
-        draggedItem.index = index;
-      }
-    },
-  });
-
-  return (
-    <div
-      ref={(node) => ref(drop(node))}
-      className="relative w-full h-[500px] cursor-pointer"
-    >
-      <img
-        src={url}
-        alt={`Photo ${index}`}
-        className="w-full h-full object-cover"
-      />
-    </div>
-  );
-}
+import DraggablePhoto from '@/components/DraggablePhoto';
 
 export default function GalleryPage() {
   const { photos, refetchPhotos, loading } = usePhotoContext();
@@ -63,6 +20,10 @@ export default function GalleryPage() {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const { user } = useAuth();
   const [photoList, setPhotoList] = useState<PhotoData[]>(photos);
+
+  useEffect(() => {
+    setPhotoList(photos);
+  }, [photos]);
 
   const openModal = (index: number) => {
     setCurrentIndex(index);
@@ -129,7 +90,10 @@ export default function GalleryPage() {
                   {user && (
                     <button
                       className="absolute top-2 right-2 flex items-center justify-center w-8 h-8 bg-red-500 text-white rounded-full p-2 hover:bg-red-700"
-                      onClick={() => handleDelete(photo.url)}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleDelete(photo.url);
+                      }}
                     >
                       ✕
                     </button>
