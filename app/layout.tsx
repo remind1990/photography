a9e96ae4659c20d3a7a3e44ab33cfd848cc926df
remind1990 <tigerstylest00@gmail.com>
2025-06-30 19:38:40 -0230

    seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,31 @@ import { fetchPhotos } from '@/lib/fetchPhotos';
 const inter = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Dubenko photography',
-  description: 'made to enjoy',
+  title: 'Dubenko Photography - St. Johns, Newfoundland and Labrador',
+  description:
+    'Professional photography services in St. Johns, Newfoundland and Labrador. Book your photoshoot today!',
+  keywords: [
+    'Photography',
+    'St. Johns',
+    'Newfoundland and Labrador',
+    'Photoshoot',
+    'Dubenko Photography',
+    'Canada',
+  ],
+  openGraph: {
+    title: 'Dubenko Photography - St. Johns, Newfoundland and Labrador',
+    description:
+      'Professional photography services in St. Johns, Newfoundland and Labrador. Book your photoshoot today!',
+    url: 'https://dubenko-olya-ph.com/',
+    images: [
+      {
+        url: 'https://dubenko-olya-ph.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Dubenko Photography',
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({
