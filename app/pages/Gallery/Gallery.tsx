38f5d5b56e7eb22623115e88476d3904348c6f91
remind1990 <tigerstylest00@gmail.com>
2025-07-05 19:49:24 -0230

    fix for photo order

--- app/pages/Gallery/Gallery.tsx
+++ app/pages/Gallery/Gallery.tsx
@@ -23,7 +23,7 @@ export default function GalleryPage() {
 
   useLayoutEffect(() => {
     console.log('GalleryPage useLayoutEffect', photos);
-    setPhotoList(photos);
+    setPhotoList([...photos].reverse());
   }, [photos]);
 
   const openModal = (index: number) => {
@@ -48,19 +48,21 @@ export default function GalleryPage() {
     const updatedPhotos = [...photoList];
     const [movedPhoto] = updatedPhotos.splice(fromIndex, 1);
     updatedPhotos.splice(toIndex, 0, movedPhoto);
-    const reorderedPhotos = updatedPhotos.map((photo, index) => ({
-      ...photo,
-      order: index,
-    }));
+
+    // Reassign "reversed" order so that top = highest number
+    const reorderedPhotos = [...updatedPhotos]
+      .reverse() // so the first shown = highest order
+      .map((photo, index) => ({
+        ...photo,
+        order: index,
+      }));
+
     setPhotoList(updatedPhotos);
+
     if (user) {
       updatePhotoOrder(reorderedPhotos)
-        .then((res) => {
-          console.log('Photo updated successfully');
-        })
-        .catch((error) => {
-          console.error('Failed to update photo', error);
-        });
+        .then(() => console.log('Photo updated successfully'))
+        .catch((error) => console.error('Failed to update photo', error));
     }
   };
 
